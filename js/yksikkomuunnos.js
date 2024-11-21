52f76e838c99b1c52d4ee3b2987e5adebd29f010
JuusoP <c3poju02@students.oamk.fi>
2024-11-21 20:32:22 +0200

    Yksikkö: Kysymyslista ja html moukkaus

---
+++ js/yksikkomuunnos.js
@@ -0,0 +1,37 @@
+const questions = [
+    {
+        question: "Montako litraa on 100 desilitraa?",
+        options: ["1 litraa", "10 litraa", "0.1 litraa", "1000 litraa"],
+        answer: "10 litraa"
+    },
+    {
+        question: "Montako millilitraa on 30 desilitraa?",
+        options: ["300 milliitraa", "3 millilitraa", "3000 millilitraa", "0.03 millilitraa"],
+        answer: "3000 millilitraa"
+    },
+    {
+        question: "Montako litraa on 12000 millilitraa?",
+        options: ["12 litraa", "1.2 litraa", "0.12 litraa", "120 litraa"],
+        answer: "12 litraa"
+    },
+    {
+        question: "Montako kilogrammaa on 700 grammaa?",
+        options: ["7 kilogramma", "0.7 kilogrammaa", "70 kilogrammaa", "0.07 kilogrammaa"],
+        answer: "0.7 kilogrammaa"
+    },
+    {
+        question: "Montako milligrammaa on 50 grammaa?",
+        options: ["5 milligrammaa", "5000 milligrammaa", "500 milligrammaa", "50000 milligrammaa"],
+        answer: "50000 milligrammaa"
+    },
+    {
+        question: "Montako milligrammaa on 0.06 kilogrammaa?",
+        options: ["600 milligrammaa", "60 milligrammaa", "60000 milligrammaa", "6 milligrammaa"],
+        answer: "60000 milligrammaa"
+    },
+    {
+        question: "Montako millilitraa on 1 ruokalusikka",
+        options: ["5 milligrammaa", "20 milligrammaa", "3 milligrammaa", "15 milligrammaa"],
+        answer: "15 milligrammaa"
+    },
+]
