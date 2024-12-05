c2421ae96425ba96647e3cdf9e2c4a2861307f5c
JuusoP <c3poju02@students.oamk.fi>
2024-12-05 17:04:01 +0200

    Yksikkö: Pisteenlaskenta ja tallennus sessionstorageen

--- js/yksikkomuunnos.js
+++ js/yksikkomuunnos.js
@@ -31,8 +31,8 @@ const kysymykset = [
     },
     {
         kysymys: "Montako milligrammaa on 0.06 kilogrammaa?",
-        valinnat: ["600 milligrammaa", "60 milligrammaa", "60000 milligrammaa", "6 milligrammaa"],
-        vastaus: "60000 milligrammaa",
+        valinnat: ["600 milligrammaa", "60 milligrammaa", "60 000 milligrammaa", "6 milligrammaa"],
+        vastaus: "60 000 milligrammaa",
         ohjeet: "1 kilogramma on 1 000 000 milligrammaa. Joten 0.06 kilogrammaa on 0.06 * 1 000 000 = 60 000 milligrammaa."
     },
     {
@@ -40,17 +40,38 @@ const kysymykset = [
         valinnat: ["5 milligrammaa", "20 milligrammaa", "3 milligrammaa", "15 milligrammaa"],
         vastaus: "15 milligrammaa",
         ohjeet: "1 ruokalusikka on 15 milligrammaa."
+    },
+    {
+        kysymys: "Montako grammaa on 2.5 kilogrammaa?",
+        valinnat: ["250 grammaa", "2 500 grammaa", "25 grammaa", "25 000 grammaa"],
+        vastaus: "2 500 grammaa",
+        ohjeet: "1 kilogramma on 1000 grammaa, joten 2.5 kilogrammaa on 2.5 * 1000 = 2500 grammaa."
+    },
+    {
+        kysymys: "Montako litraa on 2500 millilitraa?",
+        valinnat: ["2.5 litraa", "25 litraa", "0.25 litraa", "250 litraa"],
+        vastaus: "2.5 litraa",
+        ohjeet: "1 litra on 1 000 millilitraa, joten 2500 millilitraa on 2500 / 1 000 = 2.5 litraa."
+    },
+    {
+        kysymys: "Montako desilitraa on 0.75 litraa?",
+        valinnat: ["7.5 desilitraa", "75 desilitraa", "0.75 desilitraa", "750 desilitraa"],
+        vastaus: "7.5 desilitraa",
+        ohjeet: "1 litra on 10 desilitraa, joten 0.75 litraa on 0.75 * 10 = 7.5 desilitraa."
     }
-]   //Lista mikä sisältää kysymykset, vaihtoehdot sekä vastaukset//
+    
+]   //Lista mikä sisältää kysymykset, vaihtoehdot sekä vastaukset
 
-let kysymysnumero = 0   //Laskuri joka kertoo monesko kysymys on menossa//
-let pisteet = 0     //Laskuri joka laskee saadut pisteet//
+let kysymysnumero = 0   //Laskuri joka kertoo monesko kysymys on menossa
+let oikein = 0     //Laskuri joka laskee oikein saadut kysymykset
+let pisteet = 0    //Lopulliset pisteet jotka tallenetaan sessionstorageen
 
 const aloitaPeli = () => {      //Funktio jolla aloitetaan peli//
-    document.getElementById("aloitus-ruutu").style.display = "none"     //Vaihtaa näkymäksi aloitusruudun//
+    document.getElementById("aloitus-ruutu").style.display = "none"     //Vaihtaa näkymäksi aloitusruudun
     document.getElementById("kysymys-ruutu").style.display = "block"    
-    kysymysnumero = 0   //Nollaa laskurit, jos halutaan pelata peliä uudestaan//
-    pisteet = 0
+    kysymysnumero = 0   //Nollaa laskurit, jos halutaan pelata peliä uudestaan
+    oikein = 0
+    kysymykset.sort(() => Math.random() - 0.5) //Sekoittaa kysymykset satunnaiseen järjestykseen
     tuoKysymys()
 }
 
@@ -62,7 +83,7 @@ const tuoKysymys = () => {      //Funktio joka hakee seuraavan kysymyksen sekä 
     const valintaLaatikko = document.getElementById("valinta-laatikko")
     valintaLaatikko.innerHTML = ""
 
-    kysymys.valinnat.forEach(valinta => {       //Luodaan valinnoille painikkeet//
+    kysymys.valinnat.forEach(valinta => {       //Luodaan valinnoille painikkeet
         const painike = document.createElement("button")
         painike.innerText = valinta
         painike.classList.add("valinta-painike")
@@ -78,7 +99,7 @@ const valitseVastaus = (painike, valittuValinta) => {       //Tällä funktiolla
     const ohjeetLaatikko = document.getElementById("ohjeet-laatikko")
 
     if (valittuValinta === oikeaVastaus) {      //Tarkastaa vastauksen, antaa pisteen jos vastaus on oikein. Vaihtaa painikkeen väriä sen mukaan onko vastaus oikein vai väärin.
-        pisteet++
+        oikein++
         painike.style.backgroundColor = "lightgreen"
         palauteLaatikko.innerText = "Oikein!"
     } else {
@@ -105,10 +126,25 @@ const seuraavaKysymys = () => {     //Funktio joka kutsuu tuoKysymys funktiota j
     }
 }
 
-const lopetaPeli = () => {  //Funktio jolla lopetetaan peli, ja siirrytään tulosruutuun.
-    document.getElementById("kysymys-ruutu").style.display = "none" //Piilottaa kysymysruudun
-    document.getElementById("tulos-ruutu").style.display = "block"  //Näyttää tulosruudun
-    document.getElementById("tulos-teksti").innerText = `Sait ${pisteet} / ${kysymykset.length} oikein!` //Päivittää tulostekstin sen mukaan kuinka moneen käyttäjä on vastannut oikein.
+const lopetaPeli = () => {  
+    document.getElementById("kysymys-ruutu").style.display = "none"
+    document.getElementById("tulos-ruutu").style.display = "block"
+    pisteet = oikein*2 // Määrittää lopulliset pisteet
+
+    let tulosteksti = ""
+
+    if (pisteet >= 16) {
+        tulosteksti = `Uskomatonta! Sait ${oikein} / ${kysymykset.length} oikein! Olet mestari!`
+
+    } else if (pisteet >= 10) {
+        tulosteksti = `Hyvin tehty! Sait ${oikein} / ${kysymykset.length} oikein. Harjoittelemalla voit parantaa vielä enemmän.`
+
+    } else {
+        tulosteksti = `Voi ei! sait ${oikein} / ${kysymykset.length} oikein. Ei hätää, harjoittelu tekee mestarin! Kokeile uudelleen ja paranna tulostasi.`    
+    }
+
+    document.getElementById("tulos-teksti").innerText = tulosteksti
+    tallennaSessionStorageen("yksikkomuunnosPisteet", pisteet) // Tallentaa pisteet session storageen funktion avulla joka löytyy shared.js
 }
 
 const resetoiPeli = () => {     //Funktio jolla voidaan aloittaa peli uudestaan.
