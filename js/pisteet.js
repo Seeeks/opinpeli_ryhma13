baea188c32176bc5b360ce45178827907010ae63
Seeeks <okarrdess@yahoo.com>
2024-12-07 17:34:52 +0200

    pisteetsivu

--- js/pisteet.js
+++ js/pisteet.js
@@ -2,11 +2,13 @@ const yhtPisteetOutput = document.getElementById("yhtPisteet");
 
 const muistiPisteetOutput = document.getElementById("muistiPisteet");
 const yksikkomuunnosPisteetOutput = document.getElementById("yksikkoPisteet");
+const keittiovalinePisteetOutput = document.getElementById("valinePisteet");
 //Lisätkää tähän omien outputtienne referenssit. Id:t näkee pisteet.html. Niitä saa muuttaa omalta kohdaltaan jos haluaa
 
 
 document.addEventListener("DOMContentLoaded", function () {
     muistiPisteetOutput.textContent = haeTietoSessionStoragesta("muistipelinPisteet") || "--";
     yksikkomuunnosPisteetOutput.textContent = haeTietoSessionStoragesta("yksikkomuunnosPisteet") || "--";
+    keittiovalinePisteetOutput.textContent = haeTietoSessionStoragesta("keittiovalinePisteet") || "--";
     yhtPisteetOutput.textContent = laskeYhteispisteet() || "--";
 });
