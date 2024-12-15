633c2b4272b72f2e0093fe17f4072d883ee1e431
Vartsu245 <n3vaar00@students.oamk.fi>
2024-12-15 18:04:45 +0200

    Kommentointi, resepti ja muuta pientä css säätöä. Siistitty käsdittely pienemmille näytöille

--- js/pelipainike.js
+++ js/pelipainike.js
@@ -6,8 +6,11 @@ const uusipeli = () => {
     if(kierros <= 2) {
     let peli= pelinKasittely(kierros);
     
-
-    /*Kirjasta Clean code in Javascript */
+        
+    /*
+    Kirjasta Clean code in Javascript -> erinomainen teos
+    sieltä sai keinon tuohon kuinka luupata asiat lista näkyviin
+    */
     const ul = document.createElement('ul');
     ul.id = "pelaajan_lista";
 
@@ -25,6 +28,8 @@ const uusipeli = () => {
         pala.textContent = item;
 
         //https://www.w3schools.com/howto/howto_js_cascading_dropdown.asp
+        //Dropdown sovellettu sivun ohjeen mukaan.
+        //ID haasteiden ja listan hallinnan takia ei kannattanut toteuttaa erillisenä funktiona, vaikka niin olisi selkeämpi
         const alasveto = tarkistus.appendChild(document.createElement("div"));
         const valittavat = alasveto.appendChild(document.createElement("select"));
         valittavat.id = "select"+erottaja;
@@ -47,6 +52,7 @@ const uusipeli = () => {
         arvo.textContent = "Valitse";
         erottaja++;
     });
+    //Funktioiden lopussa painikkeiden hallintaa
     document.getElementById("peli_hallitse").disabled = true;
     document.getElementById("peli_hallitse").innerHTML = "Pelaa, kierros 2"
     document.getElementById("peli_tarkista").disabled = false;
@@ -54,10 +60,12 @@ const uusipeli = () => {
 
     kierros ++;
     
+    //kierroksia kaksi, joten kolmas voi aloittaa uuden
     if(kierros == 3) {
         document.getElementById("peli_hallitse").innerHTML = "Uusipeli";
     }
     
+    //varsinainen uusi kierros alkaa tästä
     if(kierros == 4) {
         document.getElementById("peli_hallitse").innerHTML = "Pelaa, kierros 1";
         document.getElementById("jarjestyspuhekupla").innerHTML ="Voi minua nakkisormea! Laitoin vahingossa reseptit pesukoneeseen kokkiveitsien kanssa ja reseptit pilkkoutuivat uudelleen"
@@ -66,17 +74,13 @@ const uusipeli = () => {
     }
     
 }
-
-function tallennaPisteet() {
-   console.log("tallenna");
-}
-
-
+//tarkistus piilotetaan tällä funktiolla, tai paremminkin poistetaan
 function piilotaTarkistus() {
     document.getElementById("tarkistus").remove();
     document.getElementById("pelaajan_lista").remove();
 }
 
+//Tällä arvotaan mikä peli tulee. Hieman satunaistamista randomilla
 function pelinKasittely(luku) {
     if (luku ==1) {
         
@@ -91,15 +95,30 @@ function pelinKasittely(luku) {
         
         switch (parseInt(arvo)) {
             case 1:
-                return ["a","b","c","d"]
+                return [
+                    "Kuori ja pilko perunat",
+                    "Laita vesi kiehumaan",
+                    "Laita perunat kiehuvaan veteen ja keitä kypsäksi",
+                    "Kaada vesi pois ja kaada tilalle sama määrä maitoa, lämmitä mutta älä keitä!"
+                ];
             break;
         
             case 2:
-                return ["b","b","c","a"]
+                return [
+                    "Kaada vesi pois ja kaada tilalle sama määrä maitoa, lämmitä mutta älä keitä!",
+                    "Kuori ja pilko perunat",
+                    "Laita perunat kiehuvaan veteen ja keitä kypsäksi",
+                    "Laita vesi kiehumaan",
+                ];
             break;
         
             case 3:
-                return ["a","d","a","b"]
+                return [
+                    "Laita vesi kiehumaan",
+                    "Kaada vesi pois ja kaada tilalle sama määrä maitoa, lämmitä mutta älä keitä!",
+                    "Kuori ja pilko perunat",
+                    "Laita perunat kiehuvaan veteen ja keitä kypsäksi"
+                ];
             break;      
 
         }
@@ -120,15 +139,37 @@ function pelinKasittely(luku) {
         
         switch (parseInt(arvo)) {
             case 1:
-                return ["a","b","c","d","e","f"]
+                return [
+                    "Tee ruistaikina vedestä, suolasta ja ruisjauhoista. Paremman sitkon leivontaa ajatellen saat, kun laitat taikinaan alkuvaiheessa pari desiä vehnäjauhoja",
+                    "Ota kukko uunista, voitele halutessa kuorta voilla. Kääri kalakukko folioon ja laske uunin lämpötila noin  150- asteeseen. Paista noin 6 tuntia",
+                    "Laita pellille kukko/ leivinpaperi ja foliot johon kukko kääritään. Paista kukkoa uunissa kunnes kuoreen tulee ruskeita laikkuja",
+                    "Laita uuni lämpiämään 225-asteeseen. Kauli taikinasta niin suuri levy, että saat muotoiltua siitä koko kalakukon",
+                    "Arvioi kalakukon pohjan koko. Laita riisiä/ohrasuurimoja/makaronia koko pohja alueelle. Lado kala (ja liha jos käytät) kerroksiin maustaen kerrokset suolalla.",
+                    "Käännä taikina kalakukon päälle ja tilkitse kuoren aukot ja kolot taikinalla"
+                ]
             break;
         
             case 2:
-                return ["b","c","d","e","f","a"]
+                return [
+                    "Käännä taikina kalakukon päälle ja tilkitse kuoren aukot ja kolot taikinalla",
+                    "Laita pellille kukko/ leivinpaperi ja foliot johon kukko kääritään. Paista kukkoa uunissa kunnes kuoreen tulee ruskeita laikkuja",
+                    "Laita uuni lämpiämään 225-asteeseen. Kauli taikinasta niin suuri levy, että saat muotoiltua siitä koko kalakukon",
+                    "Arvioi kalakukon pohjan koko. Laita riisiä/ohrasuurimoja/makaronia koko pohja alueelle. Lado kala (ja liha jos käytät) kerroksiin maustaen kerrokset suolalla.",
+                    "Ota kukko uunista, voitele halutessa kuorta voilla. Kääri kalakukko folioon ja laske uunin lämpötila noin  150- asteeseen. Paista noin 6 tuntia",
+                    "Tee ruistaikina vedestä, suolasta ja ruisjauhoista. Paremman sitkon leivontaa ajatellen saat, kun laitat taikinaan alkuvaiheessa pari desiä vehnäjauhoja"
+                    
+                ]
             break;
         
             case 3:
-                return ["c","d","e","f","a","b"]
+                return [
+                    "Ota kukko uunista, voitele halutessa kuorta voilla. Kääri kalakukko folioon ja laske uunin lämpötila noin  150- asteeseen. Paista noin 6 tuntia",
+                    "Arvioi kalakukon pohjan koko. Laita riisiä/ohrasuurimoja/makaronia koko pohja alueelle. Lado kala (ja liha jos käytät) kerroksiin maustaen kerrokset suolalla.",
+                    "Käännä taikina kalakukon päälle ja tilkitse kuoren aukot ja kolot taikinalla",
+                    "Tee ruistaikina vedestä, suolasta ja ruisjauhoista. Paremman sitkon leivontaa ajatellen saat, kun laitat taikinaan alkuvaiheessa pari desiä vehnäjauhoja",
+                    "Laita uuni lämpiämään 225-asteeseen. Kauli taikinasta niin suuri levy, että saat muotoiltua siitä koko kalakukon",
+                    "Laita pellille kukko/ leivinpaperi ja foliot johon kukko kääritään. Paista kukkoa uunissa kunnes kuoreen tulee ruskeita laikkuja"
+                ]
             break;      
 
         } 
--- js/tarkistaPainike.js
+++ js/tarkistaPainike.js
@@ -1,4 +1,6 @@
+
 let kokonaispisteet = 0;
+//tarvitaan avuksi arvioimaan kierroksen 2pisteitä
 let aateapu = 0;
 
 const tarkista = () =>{
@@ -10,21 +12,35 @@ const tarkista = () =>{
     kaninAatteet();
 
     tallennaSessionStorageen("jarjestysPisteet", kokonaispisteet)
-
 }
 
+
+// Yksinkertainen ehto tarkistaa onko kierros 1 vai 2
 function pelinTarkistus() {
-    
+    //Hieman merkkijono kikkailua, muuttujan siirtäminen js filestä toiseen vaatisi export kikkailua
     if (document.getElementById('mika_peli').innerHTML.substring(0, 9)=='Kierros 1'){
-        return ["a","b","c","d"]    
+            return [
+                "Laita vesi kiehumaan",
+                "Kuori ja pilko perunat",
+                "Laita perunat kiehuvaan veteen ja keitä kypsäksi",
+                "Kaada vesi pois ja kaada tilalle sama määrä maitoa, lämmitä mutta älä keitä!"
+            ];    
         }
 
     if (document.getElementById('mika_peli').innerHTML.substring(0, 9)=='Kierros 2') {
         aateapu = kokonaispisteet;
-        return ["a","b","c","d","e","f"]
+            return [
+                "Tee ruistaikina vedestä, suolasta ja ruisjauhoista. Paremman sitkon leivontaa ajatellen saat, kun laitat taikinaan alkuvaiheessa pari desiä vehnäjauhoja",
+                "Laita uuni lämpiämään 225-asteeseen. Kauli taikinasta niin suuri levy, että saat muotoiltua siitä koko kalakukon",
+                "Arvioi kalakukon pohjan koko. Laita riisiä/ohrasuurimoja/makaronia koko pohja alueelle. Lado kala (ja liha jos käytät) kerroksiin maustaen kerrokset suolalla.",
+                "Käännä taikina kalakukon päälle ja tilkitse kuoren aukot ja kolot taikinalla",
+                "Laita pellille kukko/ leivinpaperi ja foliot johon kukko kääritään. Paista kukkoa uunissa kunnes kuoreen tulee ruskeita laikkuja",
+                "Ota kukko uunista, voitele halutessa kuorta voilla. Kääri kalakukko folioon ja laske uunin lämpötila noin  150- asteeseen. Paista noin 6 tuntia"
+            ]
         } 
 }
  
+//Hieman väriä eri kierroksille laittamalla Kokkikani puhelemaan
 function kaninAatteet(){
     if (document.getElementById('mika_peli').innerHTML.substring(0, 9)=='Kierros 1'){
             if (kokonaispisteet==0){
@@ -59,31 +75,32 @@ function kaninAatteet(){
 
 function tarkistaja () {
     /*Haetaan pelaan vastaus listana*/
-    
+    /*Selectien sisällöt */
     let pelaajan_vastaus = document.querySelectorAll("select");
     let pelaajan_vastaus_lista = [];
    
+   /*Isossa kuvassa kannattaa karsia listoista turha, tässä tapauksessa tarvittin vain selectien arvot */
     pelaajan_vastaus.forEach(item => {
         pelaajan_vastaus_lista.push(item.value);
     });
     
+    /*Koska järjestyksiä on satunaistettua, on pelaajan arvauksen lisäksi tiedettävä mitä osaa pelaaja arpoo */
     let pelaajan_vastaus_teksti = document.querySelectorAll(".pelaajan_vastaus");
     let pelaajan_teksti_lista = [];
 
+    /*Haluamme tutkia vain pelaajan näkemää järjestystä */
     pelaajan_vastaus_teksti.forEach(item => {
         pelaajan_teksti_lista.push(item.innerHTML);
     });
 
-    //toimii tähän asti
-
     /*Tarkistetaan vastaako lista oikeaa*/
-    
+    /*Tästä eteenpäin luodaan tarkistus näkymää*/
     let peli = pelinTarkistus();
 
     const tarkistus = document.createElement("div");
     tarkistus.classList.add("peliruutu");
     tarkistus.id = "tarkistus";
-    const sisus = document.createTextNode("tarkistus")
+    const sisus = document.createTextNode("Oikea vastaus")
     tarkistus.append(sisus);
 
     const ul = document.createElement('ul');
@@ -91,7 +108,7 @@ function tarkistaja () {
     
     /*tästä eteenpäin tarkistus */
     let indeksi = 0;
-
+    /*tutkitaan pelaajan vastaus oikeaa listaa vasten */
     peli.forEach(item => {
 
         const li =ul.appendChild(document.createElement('li'));
@@ -99,20 +116,23 @@ function tarkistaja () {
         tarkistus.classList.add("tarkista_oikea")
         tarkistus.id = "tarkista_oikea"
         tarkistus.textContent = item;
-        
-
+        /*
+        Tutkitaan pelaajan näkemää sekoitettua listaa
+        Koska asiat luodaan ja luetaan järjestykseen html documenttiin
+        voidaan tutkia asioita indeksejä soveltamalla
+        */
         pelaajan_teksti_lista.forEach(item2 => {
            
             if (item2 == tarkistus.textContent){
                 let teksti_indeksi = pelaajan_teksti_lista.indexOf(item2);
                 let vastaus_indeksi = pelaajan_vastaus_lista[teksti_indeksi];
-
+                /*array alkaa aina indeksistä 0 */
                 if(indeksi == vastaus_indeksi-1) {
-                    tarkistus.style.backgroundColor = "green";
+                    tarkistus.style.backgroundColor = "rgb(77, 218, 77)";
                     kokonaispisteet++;
                 }
                 else {
-                    tarkistus.style.backgroundColor = "red";
+                    tarkistus.style.backgroundColor = "rgb(237, 117, 117)";
                 }
             }
         });
@@ -121,7 +141,7 @@ function tarkistaja () {
     });
 
     tarkistus.append(ul);
-    document.getElementById("pelin_hallinta").appendChild(tarkistus);
+    document.getElementById("pelin_hallinta").insertBefore(tarkistus, document.getElementById("ohjeruutu"));
 
     document.getElementById("kokonaispisteet").innerHTML="Kokonaispisteesi: " + kokonaispisteet;
 
